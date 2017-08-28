2ff085050a3945c4383b7d2e5d2a0589aee96f74
Aklain52 <escaflowne52@gmail.com>
2017-08-28 11:12:01 +0200

    searchfom.php - échec des recherches

--- searchform.php
+++ searchform.php
@@ -1,6 +1,16 @@
-<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+<form role="search" method="get" id="searchform" class="searchform" action="<?php bloginfo('home'); ?>/">
     <div>
-        <input type="text" value="<?php the_search_query(); ?>" name="champ de recherche" id="champ_de_recherche" />
-        <input type="submit" id="searchsubmit" value="&#xf002;" />
+        <input value="<?php the_search_query(); ?>" type="text" placeholder="Rechercher..." name="champ de recherche" id="champ_de_recherche" >
+        <input type="submit" id="searchsubmit" value="&#xf002;" >
     </div> 
 </form>
+
+<!--<div>
+<form role="search" method="get" id="searchform" class="searchform" action="http://localhost/wordpress/">
+	<div>
+		<label class="screen-reader-text" for="s">Rechercher&nbsp;:</label>
+		<input value="" name="s" id="s" type="text">
+		<input id="searchsubmit" value="Rechercher" type="submit">
+	</div>
+</form>
+</div>-->
--- sidebar-2.php.php
+++
@@ -1,3 +0,0 @@
-<div id="sidebar-widget-area" class="widget-area" role="complementary">
-    <?php dynamic_sidebar( 'sidebar-2' ); ?>
-</div>
