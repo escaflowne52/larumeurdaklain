2a4697b17544c7cb01a14eb72828dafc74d77fe2
Aklain52 <escaflowne52@gmail.com>
2017-08-28 12:48:59 +0200

    searchfom.php - centrage vertical du menu principal

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php bloginfo('home'); ?>/">
-    <div>
-        <input value="<?php the_search_query(); ?>" type="text" placeholder="Rechercher..." name="s" id="champ_de_recherche" >
+    <div id="recherche">
+        <input value="<?php the_search_query(); ?>" type="text" placeholder="Rechercher..." name="s" id="champ_de_recherche" size="1" >
         <input type="submit" id="searchsubmit" value="&#xf002;" >
     </div> 
 </form>
